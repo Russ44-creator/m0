306d85f97eb16334c75ce3c3b08e556c0b2eea73
Russ44-creator <russ1254016017@gmail.com>
2024-02-05 00:46:13 -0500

    check shellcheck

--- c/process.sh
+++ c/process.sh
@@ -3,5 +3,5 @@
 # line, convert it to lowercase ascii, and remove any stopwords 
 # useful commands: tr, iconv, grep
 
-tr -cs A-Za-z '\n' | tr 'A-Z' 'a-z' | iconv -c -t ASCII | grep -vxF -f d/stopwords.txt 
+tr -cs A-Za-z '\n' | tr '[:upper:]' '[:lower:]' | iconv -c -t ASCII | grep -vxF -f d/stopwords.txt | cat 
 # TODO replace `cat` more code...
